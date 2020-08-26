597a731554d2fb9abda8cea1b14f59a8823e5c2c
shaoqg <1119347352@qq.com>
2020-08-26 17:17:33 +0800

    Add res

---
+++ assets/Scripts/Pet/Idle.ts
@@ -0,0 +1,41 @@
+import { PetObject } from "./PetObject";
+import { Behavior } from "./Behavior";
+
+export class Idle extends Behavior {
+
+    protected _actorAnimation: cc.Animation;
+
+    getType() : string {
+        return "Idle";
+    }
+
+    init(pet:PetObject, source:string) {
+        super.init(pet, source, null);
+        this._actorAnimation = this._actor.node.getComponent<cc.Animation>(cc.Animation);
+
+    }
+
+    update(dt) {
+        let finished =  super.update(dt);
+        if (finished) {
+            this._isActive = true;
+
+            this._actorAnimation.play("pet_idle");
+            this._actorAnimation.on("finished", ()=>{
+                this._isActive = false;
+            })
+        }
+        return !this._isActive;
+    }
+
+    clean() {
+        this._stand();
+        super.clean();
+    }
+
+    _stand() {
+        if(this._actorAnimation) {
+            this._actorAnimation.stop("pet_idle");
+        }
+    }
+}
---
+++ assets/Scripts/Pet/Land.ts
@@ -0,0 +1,32 @@
+import { PetObject } from "./PetObject";
+import { Behavior } from "./Behavior";
+
+export class Land extends Behavior {
+
+    protected _actorAnimation: cc.Animation;
+
+    getType() : string {
+        return "Land";
+    }
+
+    init(pet:PetObject, source:string) {
+        super.init(pet, source, null);
+        this._actorAnimation = this._actor.node.getComponent<cc.Animation>(cc.Animation);
+
+        this._actorAnimation.play("pet_jump");
+        this._actorAnimation.on("finished", ()=>{
+            this._isActive = false;
+        }, this._actorAnimation);
+    }
+
+    clean() {
+        this._stand();
+        super.clean();
+    }
+
+    _stand() {
+        if(this._actorAnimation) {
+            this._actorAnimation.stop("pet_jump");
+        }
+    }
+}
